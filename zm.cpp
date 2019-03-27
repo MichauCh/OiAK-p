ef1aea317b53c3e498a15213c94c4ed963ded575
michau <225936@student.pwr.edu.pl>
2019-03-27 20:26:53 +0100

    mul and div for ulongs

--- zm.cpp
+++ zm.cpp
@@ -73,7 +73,26 @@ smNum sub(smNum x, smNum y){
 	return r;
 
 }
-smNum mul(smNum x, smNum y){}
+smNum mul(smNum x, smNum y){
+	smNum m;
+	if(x.sign==y.sign)
+		m.sign = 0;
+	else
+		m.sign = 1;
+
+	m.module_val = x.module_val * y.module_val;
+	return m;
+}
+smNum div(smNum x, smNum y){
+	smNum q;
+	if(x.sign==y.sign)
+		q.sign = 0;
+	else
+		q.sign = 1;
+
+	q.module_val = x.module_val / y.module_val;
+	return q;
+}
 
 int main()
 {
@@ -93,7 +112,7 @@ int main()
 	c.module_repr = c_val;
 	c.module_val = c.module_repr.to_ulong();
 
-	smNum s1, s2, s3, r1, r2, r3;
+	smNum s1, s2, s3, r1, r2, r3, m1, m2, m3, q1, q2, q3;
 
 	s1 = add(a,b);
 	s2 = add(a,c);
@@ -103,6 +122,14 @@ int main()
 	r2 = sub(a,c);
 	r3 = sub(b,c);
 
+	m1 = mul(a,b);
+	m2 = mul(a,c);
+	m3 = mul(b,c);
+
+	q1 = div(a,b);
+	q2 = div(a,c);
+	q3 = div(b,c);
+
 	std::cout<<"---------------dodawanie---------------\n";
 	std::cout<<"w 10 to "<< s1.toString_decimal()<<"\n";
 	std::cout<<"w 10 to "<< s2.toString_decimal()<<"\n";
@@ -111,6 +138,14 @@ int main()
 	std::cout<<"w 10 to "<< r1.toString_decimal()<<"\n";
 	std::cout<<"w 10 to "<< r2.toString_decimal()<<"\n";
 	std::cout<<"w 10 to "<< r3.toString_decimal()<<"\n";
+	std::cout<<"---------------mnozenie----------------\n";
+	std::cout<<"w 10 to "<< m1.toString_decimal()<<"\n";
+	std::cout<<"w 10 to "<< m2.toString_decimal()<<"\n";
+	std::cout<<"w 10 to "<< m3.toString_decimal()<<"\n";
+	std::cout<<"---------------dzielenie---------------\n";
+	std::cout<<"w 10 to "<< q1.toString_decimal()<<"\n";
+	std::cout<<"w 10 to "<< q2.toString_decimal()<<"\n";
+	std::cout<<"w 10 to "<< q3.toString_decimal()<<"\n";
 
 	return 0;
 }
