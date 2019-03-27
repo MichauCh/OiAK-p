74c8046151633b064c41615fc4247ad5cd893232
michau <225936@student.pwr.edu.pl>
2019-03-27 19:48:21 +0100

    adding on ulong

--- zm.cpp
+++ zm.cpp
@@ -5,8 +5,47 @@
 struct smNum
 {
 	bool sign;
-	std::bitset<7> module;
+	std::bitset<7> module_repr;
+	unsigned long module_val;
+
+	smNum(){
+		sign = false;
+		module_repr.reset();
+		module_val = module_repr.to_ulong();
+	}
+
+	std::string toString_decimal(){
+		std::string value = std::to_string(module_val);
+		if(sign==0){
+			std::string str("+ ");
+			return str  + value;
+		}
+		else{
+			std::string str("- ");
+			return str  + value;
+		}
+	}
 };
+
+smNum add(smNum x, smNum y){
+	smNum s;
+	if(x.sign == y.sign){ //znaki zgodne
+		s.sign = x.sign;
+		//dodawanie modulow
+		s.module_val = x.module_val + y.module_val;
+	}
+	else{ //znaki niezgodne
+		if(x.module_val >= y.module_val)
+			s.module_val = x.module_val - y.module_val;
+		else
+		{
+			s.module_val = y.module_val - x.module_val;
+		}
+	}
+	return s;
+}
+smNum sub(smNum x, smNum y){}
+smNum mul(smNum x, smNum y){}
 
 int main()
 {
@@ -16,14 +55,25 @@ int main()
 	b.sign = 0;
 	c.sign = 1;
 
-	a.module[0] = 1; a.module[5] = 1;
-	b.module.set(); //sets all bits to true
-	//std::string str = "1110111";
+	a.module_repr[0] = 1; a.module_repr[5] = 1;
+	a.module_val = a.module_repr.to_ulong();
+
+	b.module_repr.set();
+	b.module_val = b.module_repr.to_ulong();
+
 	std::bitset<7>c_val("1110111");
-	c.module = c_val;
-	std::cout << "Znak a: " << a.sign << " modul a " << a.module.to_string()<<"\n";
-	std::cout << "Znak b: " << b.sign << " modul b " << b.module.to_string()<<"\n";
-	std::cout << "Znak c: " << c.sign << " modul c " << c.module.to_string()<<"\n";
+	c.module_repr = c_val;
+	c.module_val = c.module_repr.to_ulong();
+
+	smNum s1, s2, s3;
+
+	s1 = add(a,b);
+	s2 = add(a,c);
+	s3 = add(b,c);
+
+	std::cout<<"w 10 to "<< s1.toString_decimal()<<"\n";
+	std::cout<<"w 10 to "<< s2.toString_decimal()<<"\n";
+	std::cout<<"w 10 to "<< s3.toString_decimal()<<"\n";
 
 	return 0;
 }
